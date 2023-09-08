75cbae555efefe1cd614911ea645a5d113be05c9
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-09-08 12:07:39 +0300

    add info

--- react-personal-portfolio/src/data/projectsData.js
+++ react-personal-portfolio/src/data/projectsData.js
@@ -60,60 +60,123 @@
 // export default projectsData;
 
 
+// const projectsData = [
+// 	{
+// 		title: "React Calculator",
+// 		caption: "A project I created during the fifth course of Meta Front-End Certification in 2023.",
+// 		description: "This web application serves as a React calculator, showcasing my skills in responsive design and front-end technologies.",
+// 		getImageSrc: () => require("../images/photo1.jpg"),
+// 		url: "https://github.com/fake-username/project01",
+// 	},
+// 	{
+// 		title: "Memory Game",
+// 		caption: "A JavaScript-based game I developed to improve my skills in 2022.",
+// 		description: "This memory game challenges players to match pairs of identical animal images, demonstrating my proficiency in JavaScript.",
+// 		getImageSrc: () => require("../images/photo2.jpg"),
+// 		url: "https://github.com/fake-username/project02",
+// 	},
+// 	{
+// 		title: "Restaurant Website",
+// 		caption: "A Bootstrap-based landing page I designed for a restaurant project in 2023.",
+// 		description: "This restaurant website features diverse menus, maps, and online reservation functionality, highlighting my expertise in Bootstrap.",
+// 		getImageSrc: () => require("../images/photo3.jpg"),
+// 		url: "https://github.com/fake-username/project03",
+// 	},
+// 	{
+// 		title: "Construction Firm",
+// 		caption: "A Bootstrap-based landing page I crafted for a construction firm in 2023.",
+// 		description: "This landing page showcases construction projects and includes work cost calculations, underscoring my proficiency in Bootstrap.",
+// 		getImageSrc: () => require("../images/photo4.jpg"),
+// 		url: "https://github.com/fake-username/project04",
+// 	},
+// 	{
+// 		title: "Architecture Portfolio",
+// 		caption: "A JavaScript-based landing page I developed to showcase architectural projects in 2022.",
+// 		description: "This landing page exhibits various built architecture projects, highlighting my JavaScript skills.",
+// 		getImageSrc: () => require("../images/photo5.jpg"),
+// 		url: "https://github.com/fake-username/project05",
+// 	},
+// 	{
+// 		title: "Travel Goo",
+// 		caption: "A website I built for a travel agency in 2022, using JavaScript and SCSS.",
+// 		description: "This site enables hotel bookings, route planning, and offers a shared gallery, showcasing my proficiency in JavaScript and SCSS.",
+// 		getImageSrc: () => require("../images/photo6.jpg"),
+// 		url: "https://github.com/fake-username/project06",
+// 	},
+// 	{
+// 		title: "Reseller Business",
+// 		caption: "A website I designed for a reseller business in 2022, using JavaScript and SCSS.",
+// 		description: "This website provides insights into financial routes through dashboards, demonstrating my skills in JavaScript and SCSS.",
+// 		getImageSrc: () => require("../images/photo7.jpg"),
+// 		url: "https://github.com/fake-username/project07",
+// 	},
+// 	{
+// 		title: "Health Clinic",
+// 		caption: "An HTML and SCSS-based website I developed for a private clinic in 2022.",
+// 		description: "This website allows doctor's visit bookings and online consultations, showcasing my HTML and SCSS skills.",
+// 		getImageSrc: () => require("../images/photo8.jpg"),
+// 		url: "https://github.com/fake-username/project08",
+// 	},
+// ];
+
+// export default projectsData;
+
+
+
 const projectsData = [
 	{
 		title: "React Calculator",
 		caption: "A project I created during the fifth course of Meta Front-End Certification in 2023.",
-		description: "This web application serves as a React calculator, showcasing my skills in responsive design and front-end technologies.",
+		description: "This web application, developed in React, serves as a fully-functional calculator. It demonstrates my proficiency in responsive design and modern front-end technologies. I completed this project as part of my Meta Front-End Certification.",
 		getImageSrc: () => require("../images/photo1.jpg"),
 		url: "https://github.com/fake-username/project01",
 	},
 	{
 		title: "Memory Game",
-		caption: "A JavaScript-based game I developed to improve my skills in 2023.",
-		description: "This memory game challenges players to match pairs of identical animal images, demonstrating my proficiency in JavaScript.",
+		caption: "A JavaScript-based game I developed to improve my skills in 2022.",
+		description: "This interactive memory game, built entirely in JavaScript, challenges players to match pairs of identical animal images. It showcases my strong command of JavaScript and my dedication to honing my programming skills. I initiated this project as a personal learning endeavor in 2022.",
 		getImageSrc: () => require("../images/photo2.jpg"),
 		url: "https://github.com/fake-username/project02",
 	},
 	{
 		title: "Restaurant Website",
 		caption: "A Bootstrap-based landing page I designed for a restaurant project in 2023.",
-		description: "This restaurant website features diverse menus, maps, and online reservation functionality, highlighting my expertise in Bootstrap.",
+		description: "This elegant restaurant website, created using Bootstrap, features a variety of menus, interactive maps, and online reservation capabilities. My design and front-end development expertise shine through in this project, completed in 2023 as part of a restaurant's online presence enhancement.",
 		getImageSrc: () => require("../images/photo3.jpg"),
 		url: "https://github.com/fake-username/project03",
 	},
 	{
 		title: "Construction Firm",
 		caption: "A Bootstrap-based landing page I crafted for a construction firm in 2023.",
-		description: "This landing page showcases construction projects and includes work cost calculations, underscoring my proficiency in Bootstrap.",
+		description: "This dynamic landing page, developed with Bootstrap, prominently showcases construction projects and includes advanced work cost calculations. My Bootstrap proficiency and keen attention to detail are evident in this project, executed in 2023 to boost the online presence of a construction firm.",
 		getImageSrc: () => require("../images/photo4.jpg"),
 		url: "https://github.com/fake-username/project04",
 	},
 	{
 		title: "Architecture Portfolio",
-		caption: "A JavaScript-based landing page I developed to showcase architectural projects in 2023.",
-		description: "This landing page exhibits various built architecture projects, highlighting my JavaScript skills.",
+		caption: "A JavaScript-based landing page I developed to showcase architectural projects in 2022.",
+		description: "This captivating landing page, built with JavaScript, serves as a stunning portfolio for various built architecture projects. My strong JavaScript skills are evident in this project, designed to exhibit architectural excellence, completed in 2022.",
 		getImageSrc: () => require("../images/photo5.jpg"),
 		url: "https://github.com/fake-username/project05",
 	},
 	{
 		title: "Travel Goo",
-		caption: "A website I built for a travel agency in 2023, using JavaScript and SCSS.",
-		description: "This site enables hotel bookings, route planning, and offers a shared gallery, showcasing my proficiency in JavaScript and SCSS.",
+		caption: "A website I built for a travel agency in 2022, using JavaScript and SCSS.",
+		description: "This feature-rich travel website, developed with JavaScript and SCSS, offers users the ability to book hotels, plan routes, and share their travel experiences. My mastery of JavaScript and SCSS shines in this project, designed to enhance the online presence of a travel agency in 2022.",
 		getImageSrc: () => require("../images/photo6.jpg"),
 		url: "https://github.com/fake-username/project06",
 	},
 	{
 		title: "Reseller Business",
-		caption: "A website I designed for a reseller business in 2023, using JavaScript and SCSS.",
-		description: "This website provides insights into financial routes through dashboards, demonstrating my skills in JavaScript and SCSS.",
+		caption: "A website I designed for a reseller business in 2022, using JavaScript and SCSS.",
+		description: "This reseller business website, created using JavaScript and SCSS, provides valuable insights into financial routes through intuitive dashboards. My expertise in JavaScript and SCSS is evident in this project, designed to empower the reseller business with data-driven solutions in 2022.",
 		getImageSrc: () => require("../images/photo7.jpg"),
 		url: "https://github.com/fake-username/project07",
 	},
 	{
 		title: "Health Clinic",
-		caption: "An HTML and SCSS-based website I developed for a private clinic in 2023.",
-		description: "This website allows doctor's visit bookings and online consultations, showcasing my HTML and SCSS skills.",
+		caption: "An HTML and SCSS-based website I developed for a private clinic in 2022.",
+		description: "This user-friendly private clinic website, developed with HTML and SCSS, enables patients to book doctor's appointments and access online consultations. My proficiency in HTML and SCSS is on display in this project, designed to enhance the digital presence of a private clinic in 2022.",
 		getImageSrc: () => require("../images/photo8.jpg"),
 		url: "https://github.com/fake-username/project08",
 	},
