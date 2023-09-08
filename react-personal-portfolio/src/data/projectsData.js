d9fe6f6c088522f2b6b43ac08eaeef441744d68e
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-09-08 11:17:32 +0300

    fix titles, captions, descriptions

--- react-personal-portfolio/src/data/projectsData.js
+++ react-personal-portfolio/src/data/projectsData.js
@@ -1,57 +1,119 @@
+// const projectsData = [
+// 	{
+// 		title: "React Calculator",
+// 		description:
+// 			"React calculator from the fifth course of Meta Front-End Certification",
+// 		getImageSrc: () => require("../images/photo1.jpg"),
+// 		url: "https://github.com/fake-username/project01",
+// 	},
+// 	{
+// 		title: "Memory game",
+// 		description:
+// 			"Javascript game based on seeking two identical images of animals",
+// 		getImageSrc: () => require("../images/photo2.jpg"),
+// 		url: "https://github.com/fake-username/project02",
+// 	},
+// 	{
+// 		title: "Restaurant site",
+// 		description:
+// 			"Bootstrap based landing page for restaurant with different menus, maps and ability to book a seat online ",
+// 		getImageSrc: () => require("../images/photo3.jpg"),
+// 		url: "https://github.com/fake-username/project03",
+// 	},
+// 	{
+// 		title: "Construct firm",
+// 		description:
+// 			"Bootstrap based landing page for constructing firm with projects and calculations of work cost",
+// 		getImageSrc: () => require("../images/photo4.jpg"),
+// 		url: "https://github.com/fake-username/project04",
+// 	},
+// 	{
+// 		title: "Box company",
+// 		description:
+// 			"Javascript based landing page for architecture firm with gallery of various built architecture projects",
+// 		getImageSrc: () => require("../images/photo5.jpg"),
+// 		url: "https://github.com/fake-username/project05",
+// 	},
+// 	{
+// 		title: "Travel Goo",
+// 		description:
+// 			"Javascript and SCSS based site for travel agency with ability to book hotels, build routes and share gallery",
+// 		getImageSrc: () => require("../images/photo6.jpg"),
+// 		url: "https://github.com/fake-username/project06",
+// 	},
+// 	{
+// 		title: "Business agency",
+// 		description:
+// 			"Javascript and SCSS based site for reseller business with ability to check money routes on dashboards",
+// 		getImageSrc: () => require("../images/photo7.jpg"),
+// 		url: "https://github.com/fake-username/project07",
+// 	},
+// 	{
+// 		title: "Health clinic",
+// 		description:
+// 			"HTML and SCSS based site for private clinic with the ability to book a chosed doctor's visit and online consultations",
+// 		getImageSrc: () => require("../images/photo8.jpg"),
+// 		url: "https://github.com/fake-username/project08",
+// 	},
+// ];
+
+// export default projectsData;
+
+
 const projectsData = [
 	{
 		title: "React Calculator",
-		description:
-			"React calculator from the fifth course of Meta Front-End Certification",
+		caption: "A project I created during the fifth course of Meta Front-End Certification in 2023.",
+		description: "This web application serves as a React calculator, showcasing my skills in responsive design and front-end technologies.",
 		getImageSrc: () => require("../images/photo1.jpg"),
 		url: "https://github.com/fake-username/project01",
 	},
 	{
-		title: "Memory game",
-		description:
-			"Javascript game based on seeking two identical images of animals",
+		title: "Memory Game",
+		caption: "A JavaScript-based game I developed to improve my skills in 2023.",
+		description: "This memory game challenges players to match pairs of identical animal images, demonstrating my proficiency in JavaScript.",
 		getImageSrc: () => require("../images/photo2.jpg"),
 		url: "https://github.com/fake-username/project02",
 	},
 	{
-		title: "Restaurant site",
-		description:
-			"Bootstrap based landing page for restaurant with different menus, maps and ability to book a seat online ",
+		title: "Restaurant Website",
+		caption: "A Bootstrap-based landing page I designed for a restaurant project in 2023.",
+		description: "This restaurant website features diverse menus, maps, and online reservation functionality, highlighting my expertise in Bootstrap.",
 		getImageSrc: () => require("../images/photo3.jpg"),
 		url: "https://github.com/fake-username/project03",
 	},
 	{
-		title: "Construct firm",
-		description:
-			"Bootstrap based landing page for constructing firm with projects and calculations of work cost",
+		title: "Construction Firm",
+		caption: "A Bootstrap-based landing page I crafted for a construction firm in 2023.",
+		description: "This landing page showcases construction projects and includes work cost calculations, underscoring my proficiency in Bootstrap.",
 		getImageSrc: () => require("../images/photo4.jpg"),
 		url: "https://github.com/fake-username/project04",
 	},
 	{
-		title: "Box company",
-		description:
-			"Javascript based landing page for architecture firm with gallery of various built architecture projects",
+		title: "Architecture Portfolio",
+		caption: "A JavaScript-based landing page I developed to showcase architectural projects in 2023.",
+		description: "This landing page exhibits various built architecture projects, highlighting my JavaScript skills.",
 		getImageSrc: () => require("../images/photo5.jpg"),
 		url: "https://github.com/fake-username/project05",
 	},
 	{
 		title: "Travel Goo",
-		description:
-			"Javascript and SCSS based site for travel agency with ability to book hotels, build routes and share gallery",
+		caption: "A website I built for a travel agency in 2023, using JavaScript and SCSS.",
+		description: "This site enables hotel bookings, route planning, and offers a shared gallery, showcasing my proficiency in JavaScript and SCSS.",
 		getImageSrc: () => require("../images/photo6.jpg"),
 		url: "https://github.com/fake-username/project06",
 	},
 	{
-		title: "Business agency",
-		description:
-			"Javascript and SCSS based site for reseller business with ability to check money routes on dashboards",
+		title: "Reseller Business",
+		caption: "A website I designed for a reseller business in 2023, using JavaScript and SCSS.",
+		description: "This website provides insights into financial routes through dashboards, demonstrating my skills in JavaScript and SCSS.",
 		getImageSrc: () => require("../images/photo7.jpg"),
 		url: "https://github.com/fake-username/project07",
 	},
 	{
-		title: "Health clinic",
-		description:
-			"HTML and SCSS based site for private clinic with the ability to book a chosed doctor's visit and online consultations",
+		title: "Health Clinic",
+		caption: "An HTML and SCSS-based website I developed for a private clinic in 2023.",
+		description: "This website allows doctor's visit bookings and online consultations, showcasing my HTML and SCSS skills.",
 		getImageSrc: () => require("../images/photo8.jpg"),
 		url: "https://github.com/fake-username/project08",
 	},
